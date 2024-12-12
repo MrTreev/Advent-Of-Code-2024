d8acb1902827c5994841422a6c43ebadcdfc2a59
MrTreev <mr.treev@gmail.com>
2024-12-12 15:59:20 +1100

    Yeee

--- src/day5.cpp
+++ src/day5.cpp
@@ -1,4 +1,7 @@
 #include "util.h"
+#include <algorithm>
+#include <cassert>
+#include <stdexcept>
 #include <vector>
 
 namespace {
@@ -10,10 +13,6 @@ public:
     explicit Ordering(const std::string& line)
         : m_first(aoc::string::char_to_uint(line[0], line[1]))
         , m_second(aoc::string::char_to_uint(line[3], line[4])) {}
-
-    explicit operator std::string() const {
-        return std::format("{}|{}", m_first, m_second);
-    }
 
     [[nodiscard]]
     const uint8_t& first() const {
@@ -27,7 +26,8 @@ public:
 };
 
 class Update {
-    std::vector<uint8_t> m_buffer;
+    using item_t = uint8_t;
+    std::vector<item_t> m_buffer;
 
 public:
     explicit Update(const std::string& line) {
@@ -40,24 +40,64 @@ public:
 
     explicit operator std::string() const {
         std::string retval{};
-        for (uint8_t item: m_buffer) {
-            retval.append(std::to_string(item));
-            retval.push_back(',');
+        for (item_t item: m_buffer) {
+            retval.append(std::format("{:>2},", item));
         }
         return retval;
     }
 
-    bool satisfies(const Ordering& ord) {
-        const size_t pos_first{};
-        const size_t pos_second{};
-        if () {}
+    [[nodiscard]]
+    bool satisfies(const Ordering& ord) const {
+        size_t pos_first{0};
+        size_t pos_second{0};
+        for (size_t idx{0}; idx < m_buffer.size(); ++idx) {
+            if (m_buffer[idx] == ord.first()) {
+                pos_first = idx;
+            }
+            if (m_buffer[idx] == ord.second()) {
+                pos_second = idx;
+            }
+            if ((pos_first != 0) && (pos_second != 0)) {
+                return (pos_first < pos_second);
+            }
+        }
+        return true;
     }
 
     [[nodiscard]]
-    const uint8_t& operator[](size_t idx) const {
-        return m_buffer[idx];
+    const item_t& mid() const {
+        return m_buffer[m_buffer.size() / 2];
+    }
+
+    size_t find(item_t f_item, size_t start = 0) {
+        for (size_t idx{start}; idx < m_buffer.size(); ++idx) {
+            if (m_buffer[idx] == f_item) {
+                return idx;
+            }
+        }
+        throw std::runtime_error(std::format("no item '{}' found", f_item));
+    }
+
+    void reorder(const Ordering& ord) {
+        size_t idx_second = find(ord.second());
+        size_t idx_first  = find(ord.first(), idx_second);
+        for (size_t idx{idx_second}; idx < idx_first; ++idx) {
+            const item_t tmp_item = m_buffer[idx];
+            m_buffer[idx]         = m_buffer[idx + 1];
+            m_buffer[idx + 1]     = tmp_item;
+        }
     }
 };
+
+bool all_satisfies(const Update& update, const std::vector<Ordering>& orders) {
+    return (std::all_of(
+        orders.cbegin(),
+        orders.cend(),
+        [update](const Ordering& ooo) {
+        return update.satisfies(ooo);
+    }
+    ));
+}
 
 } // namespace
 
@@ -74,10 +114,33 @@ void aoc::run() {
             updates.emplace_back(line);
         }
     }
-    for (const auto& order: orders) {
-        aoc::print(std::string(order));
-    }
+
+    std::vector<Update> bad_updates{};
+
     for (const auto& update: updates) {
-        aoc::print(std::string(update));
+        if (all_satisfies(update, orders)) {
+            aoc::debug("good: {}", std::string(update));
+            aoc::debug("mid:  {}", std::to_string(update.mid()));
+            aoc::part1 += update.mid();
+        } else {
+            bad_updates.emplace_back(update);
+        }
+    }
+
+    for (auto& update: bad_updates) {
+        while (!all_satisfies(update, orders)) {
+            for (const auto& order: orders) {
+                if (!update.satisfies(order)) {
+                    update.reorder(order);
+                }
+            }
+        }
+        aoc::part2 += update.mid();
+        aoc::debug("sort: {}", std::string(update));
+        aoc::debug("mid:  {}", std::to_string(update.mid()));
+    }
+
+    for (const auto& update: bad_updates) {
+        assert(all_satisfies(update, orders));
     }
 }
--- src/day6.cpp
+++ src/day6.cpp
@@ -1,5 +1,239 @@
 #include "util.h"
+#include <algorithm>
+#include <cassert>
+#include <utility>
+#include <vector>
+
+namespace {
+constexpr size_t AXIS_SIZE{130}; // 10 for test, 130 for full
+enum class Direction : uint8_t { N, E, S, W };
+
+std::string to_string(Direction dir) {
+    switch (dir) {
+    case Direction::N: return "N";
+    case Direction::E: return "S";
+    case Direction::S: return "E";
+    case Direction::W: return "W";
+    }
+};
+
+class Coordinate {
+    size_t m_row;
+    size_t m_col;
+
+public:
+    constexpr Coordinate(size_t row, size_t col)
+        : m_row(row)
+        , m_col(col) {}
+
+    Coordinate operator+(const Direction& dir) const {
+        switch (dir) {
+        case Direction::N: return {m_row - 1, m_col};
+        case Direction::E: return {m_row, m_col + 1};
+        case Direction::S: return {m_row + 1, m_col};
+        case Direction::W: return {m_row, m_col - 1};
+        }
+    }
+
+    explicit operator std::string() const {
+        return std::format("({}, {})", m_row, m_col);
+    }
+
+    [[nodiscard]]
+    size_t x() const {
+        return m_row;
+    }
+
+    [[nodiscard]]
+    size_t y() const {
+        return m_col;
+    }
+
+    bool operator==(const Coordinate& other) const = default;
+
+    bool operator<(const Coordinate& other) const {
+        return (
+            std::cmp_less(m_row, other.m_row)
+            || std::cmp_less(m_col, other.m_col)
+        );
+    }
+
+    bool operator>(const Coordinate& other) const {
+        return (m_row > other.m_row || m_col > other.m_col);
+    }
+
+    bool operator>=(const Coordinate& other) const {
+        return (m_row >= other.m_row || m_col >= other.m_col);
+    }
+
+    bool operator<=(const Coordinate& other) const {
+        return (m_row <= other.m_row || m_col <= other.m_col);
+    }
+};
+
+class Map {
+private:
+    static constexpr size_t          WIDTH{AXIS_SIZE};
+    static constexpr size_t          HEIGHT{AXIS_SIZE};
+    std::array<char, HEIGHT * WIDTH> m_buffer{0};
+
+    static size_t idx(size_t row, size_t col) {
+        if (row >= HEIGHT) {
+            throw std::runtime_error(
+                std::format("row {} out of bounds, max: {}", row, HEIGHT)
+            );
+        }
+        if (col >= WIDTH) {
+            throw std::runtime_error(
+                std::format("col {} out of bounds, max: {}", col, WIDTH)
+            );
+        }
+        return (row * WIDTH) + col;
+    }
+
+    char& get_mut(size_t row, size_t col) {
+        return m_buffer[idx(row, col)]; // NOLINT
+    }
+
+public:
+    explicit Map(std::ifstream& infile) {
+        std::string line;
+        for (size_t row{0}; row < HEIGHT; ++row) {
+            std::getline(infile, line);
+            for (size_t col{0}; col < WIDTH; ++col) {
+                get_mut(row, col) = line[col];
+            }
+        }
+    }
+
+    [[nodiscard]]
+    const char& get_item(size_t row, size_t col) const {
+        return m_buffer[idx(row, col)]; // NOLINT
+    }
+
+    [[nodiscard]]
+    const char& get_item(const Coordinate& coord) const {
+        return m_buffer[idx(coord.x(), coord.y())]; // NOLINT
+    }
+
+    static constexpr size_t width() {
+        return WIDTH;
+    }
+
+    static constexpr size_t height() {
+        return HEIGHT;
+    }
+
+    explicit operator std::string() const {
+        std::string outstr{};
+        for (size_t row{0}; row < HEIGHT; ++row) {
+            for (size_t col{0}; col < WIDTH; ++col) {
+                char item = get_item(row, col);
+                outstr.push_back(item);
+            }
+            outstr.push_back('\n');
+        }
+        return outstr;
+    }
+};
+
+class Guard {
+    Direction  m_facing{Direction::N};
+    Coordinate m_position{0, 0};
+
+public:
+    explicit Guard(const Map& map) {
+        for (size_t row{0}; row < Map::height(); ++row) {
+            for (size_t col{0}; col < Map::width(); ++col) {
+                if (map.get_item(row, col) == '^') {
+                    m_position = {row, col};
+                    return;
+                }
+            }
+        }
+        throw std::runtime_error("AAAA");
+    }
+
+    void turn() {
+        switch (m_facing) {
+        case Direction::N: m_facing = Direction::E; return;
+        case Direction::E: m_facing = Direction::S; return;
+        case Direction::S: m_facing = Direction::W; return;
+        case Direction::W: m_facing = Direction::N; return;
+        }
+    }
+
+    bool check_next() {
+        constexpr Coordinate max_pos = {Map::width(), Map::height()};
+        return !((m_position + m_facing) > max_pos);
+    }
+
+    void next(const Map& map) {
+        if (map.get_item(m_position + m_facing) == '#') {
+            turn();
+        } else {
+            m_position = (m_position + m_facing);
+        }
+    }
+
+    const Coordinate& position() {
+        return m_position;
+    }
+
+    explicit operator std::string() const {
+        return std::format(
+            "{}: {}",
+            std::string(m_position),
+            to_string(m_facing)
+        );
+    }
+};
+
+bool in(auto item, const std::vector<decltype(item)>& vec) {
+    return std::ranges::any_of(vec.begin(), vec.end(), [item](auto cmp) {
+        return cmp == item;
+    });
+}
+
+void part1() {
+    std::ifstream instream{aoc::file::day_stream(6)}; // NOLINT(*-magic-numbers)
+    std::vector<Coordinate> coords{};
+    Map                     map{instream};
+    Guard                   guard{map};
+    coords.push_back(guard.position());
+
+    while (guard.check_next()) {
+        guard.next(map);
+        if (!in(guard.position(), coords)) {
+            coords.push_back(guard.position());
+        }
+    }
+    aoc::part1 = coords.size();
+    assert(aoc::part1 == 5145UL);
+}
+
+void part2() {
+    std::ifstream instream{aoc::file::day_stream(6)}; // NOLINT(*-magic-numbers)
+    std::vector<Coordinate> coords{};
+    Map                     map{instream};
+    Guard                   guard{map};
+    coords.push_back(guard.position());
+
+    while (guard.check_next()) {
+        guard.next(map);
+        if (!in(guard.position(), coords)) {
+            coords.push_back(guard.position());
+        }
+        aoc::debug(std::string(guard));
+    }
+    for (Coordinate coord: coords) {
+        aoc::debug(std::string(coord));
+    }
+}
+
+} // namespace
 
 void aoc::run() {
-    std::ifstream instream{aoc::file::day_stream(6)}; // NOLINT(*-magic-numbers)
+    ::part1();
+    ::part2();
 }
